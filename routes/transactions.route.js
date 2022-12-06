8ef00b18e1b8d276e61b2961ca47196dc41516a1
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2022-12-06 07:46:37 +0700

    adding all authentication to routes that are needed

--- routes/transactions.route.js
+++ routes/transactions.route.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const transactionController = require('../controller/transactions.controller')
+const { authenticateUser } = require('../middleware/authorization')
 
 const transactionRouter = Router()
 
-transactionRouter.get('/', transactionController.getAllTransactions)
-transactionRouter.get('/:userId', transactionController.getTransactionByUserId)
+transactionRouter.get('/', authenticateUser,transactionController.getAllTransactions)
+transactionRouter.get('/:userId', authenticateUser,transactionController.getTransactionByUserId)
 
 module.exports = transactionRouter
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,11 +5,11 @@ const {authenticateUser} = require('../middleware/authorization')
 const userRouter = Router()
 
 userRouter.get('/' , authenticateUser,userController.getAllUser)
-userRouter.get('/:userId', userController.getUserById)
-userRouter.put('/:userId', userController.editUser)
-userRouter.delete('/:userId', userController.deleteUser)
-userRouter.post('/signup', userController.signUp)
-userRouter.post('/checkout', userController.checkout)
-userRouter.put('/payment/:transactionId', userController.payment)
+userRouter.get('/:userId', authenticateUser,userController.getUserById)
+userRouter.put('/:userId', authenticateUser,userController.editUser)
+userRouter.delete('/:userId', authenticateUser,userController.deleteUser)
+userRouter.post('/signup', authenticateUser,userController.signUp)
+userRouter.post('/checkout', authenticateUser,userController.checkout)
+userRouter.put('/payment/:transactionId', authenticateUser,userController.payment)
 
 module.exports = userRouter
